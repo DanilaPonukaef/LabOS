1894774dcc04527682a543380996c79690cb02c4
DanilaPonukaef <58853263+DanilaPonukaef@users.noreply.github.com>
2019-12-20 17:37:12 +0300

    Update main.c

--- 3ALab/main.c
+++ 3ALab/main.c
@@ -11,7 +11,7 @@ char* set_time(struct tm *u)
 {
     char s[40];
     char *tmp;
-    for (int i = 0; i<40; i++) s[i] = 0;
+    memset( s, 0, 40 );
     int length = strftime(s, 40, "%d.%m.%Y %H:%M:%S, %A", u);
     tmp = (char*)malloc(sizeof(s));
     strcpy(tmp, s);
